7dfbcd3237811fc184e70bf925dc42efde46d217
EduardoGarcia270402 <eduardo27david04@gmail.com>
2026-06-08 10:59:17 -0500

    atributos

--- vehiculos/src/vehiculos/entities/vehiculo.entity.ts
+++ vehiculos/src/vehiculos/entities/vehiculo.entity.ts
@@ -1,1 +1,23 @@
-export class Vehiculo {}
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class Vehiculo {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({unique:true})
+    placa!: string;
+
+    @Column()
+    marca!:string;
+
+    @Column()
+    modelo!: string;
+
+    @Column()
+    color!: string;
+
+    @Column()
+    anio!: number;
+}
+
